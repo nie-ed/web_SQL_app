400de2b21764a845ec507bb96ac6a9cb865c1a4d
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2026-02-18 16:29:40 +0200

    Some experimentation with sqlite

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask
-from flask import render_template, request
+from flask import redirect, render_template, request
+import db
 
 app = Flask(__name__)
 
-@app.route("/form")
-def form():
-    return render_template("form.html")
+@app.route("/")
+def index():
+    messages = db.query("SELECT content FROM messages")
+    count = len(messages)
+    return render_template("index.html", count=count, messages=messages)
 
-@app.route("/result", methods=["POST"])
-def result():
-    message = request.form["message"]
-    return render_template("result.html", message=message)
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]    
+    db.execute("INSERT INTO messages (content) VALUES (?)", [content])
+    return redirect("/")
